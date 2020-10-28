143946915a5db989234ee1c064c2e94c5a4978e0
Anirban Aditya Halder <71653026+banboy38@users.noreply.github.com>
2020-10-28 22:53:15 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
     $servername = "localhost";  
     $username = "root";  
-    $password = "4761";  <!-- enter the password and credentials of your own pc -->
+    $password = "4761";  // enter the password and credentials of your own pc
     $conn = mysqli_connect ($servername , $username , $password) or die("unable to connect to host");  
     $sql = mysqli_select_db ($conn, 'studentfinal') or die("unable to connect to database");
 
